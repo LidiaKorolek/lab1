83c2420982178cf4d31627ab598f9d5437e49411
LidiaKorolek <koroleva-lida@bk.ru>
2020-05-15 01:43:51 +0800

    Moving a function PerimeterTriangle

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -5,13 +5,6 @@
 
 using namespace std;
 
-double PerimeterTriangle(double x, double y, double x1, double y1, double x2, double y2) {
-    double perimeter = sqrt(pow(x1 - x, 2) + pow(y1 - y, 2));
-    perimeter += sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    perimeter += sqrt(pow(x - x2, 2) + pow(y - y2, 2));
-    return perimeter;
-}
-
 double AreaTriangle(double x, double y, double x1, double y1, double x2, double y2) {
     double S = abs((x1 - x) * (y2 - y) - (x2 - x) * (y1 - y));
     return S;
